6ab1696982b765e70f868de5eb75a3a0c90a1870
IlyaDzen <ilix66@gmail.com>
2022-09-23 16:21:27 +0300

    trash

--- weight.rb
+++ weight.rb
@@ -7,10 +7,10 @@ height = gets.chomp
 puts "what is your weight?"
 weight = gets.chomp
 
-i_weight = ((height.to_i-110)*1.15)
+ideal_weight = ((height.to_i-110) * 1.15)
 
-if weight.to_i - i_weight>=0
-  puts "#{name},Your ideal weight is #{(height.to_i-110)*1.15}"
+if weight.to_i - ideal_weight >= 0
+  puts "#{name},Your ideal weight is #{(height.to_i - 110) * 1.15}"
 else 
   puts "Your weight is optimal)"
 end
