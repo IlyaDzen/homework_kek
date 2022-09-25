c26913471726fa8ded40649caa06958cfea2b0ce
IlyaDzen <ilix66@gmail.com>
2022-09-25 13:32:09 +0300

    trash

--- square.rb
+++ square.rb
@@ -8,13 +8,13 @@ puts "third side"
 c = gets.chomp.to_i
 
 if a == b and c == a
-	puts "triange is equal"
+  puts "triange is equal"
 else
-	if a == b or a == c or b == c
-	 puts "triange is isosceles"
-	elsif a > b and a > c  and a * a == b * b + c * c
-	elsif b > a and b. > c and b * b == a * a + c * c
-	elsif c > b and c > a  and c * c == b * b + a * a
-	 puts "rectangular"	
-	end
+  if a == b or a == c or b == c
+   puts "triange is isosceles"
+  elsif a > b and a > c  and a * a == b * b + c * c
+  elsif b > a and b. > c and b * b == a * a + c * c
+  elsif c > b and c > a  and c * c == b * b + a * a
+   puts "rectangular"	
+  end
 end
