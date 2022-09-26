75c0bd7447ebbb1fc98fccad633272245c21848f
IlyaDzen <ilix66@gmail.com>
2022-09-26 15:12:53 +0300

    trash

--- 2/Part2_task1.rb
+++ 2/Part2_task1.rb
@@ -1,9 +1,12 @@
-months = {'Jan' => 31, 'Feb' => 28, 'Mar' => 31, 'Apr'=> 30, 'May' => 31, 'Jun' => 30, 'Jul' => 31, 'Aug' => 31, 'Sen' => 30, 'Oct' => 31, 'Nov' => 30, 'Dec' => 31}
+months = {
+  'Jan' => 31, 'Feb' => 28, 'Mar' => 31, 'Apr'=> 30, 'May' => 31, 'Jun' => 30, 
+  'Jul' => 31, 'Aug' => 31, 'Sen' => 30, 'Oct' => 31, 'Nov' => 30, 'Dec' => 31
+}
 
 ind = 0
 while ind < 12 do
-  if months.values[0+ind] == 30
-  puts months.keys[0+ind]
+  if months.values[ind] == 30
+    puts months.keys[ind]
   end
-ind +=1
+  ind += 1
 end
