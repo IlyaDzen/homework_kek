c351971650562cc6310d9f6cca2f5a6a65a6bd14
IlyaDzen <ilix66@gmail.com>
2022-10-01 16:24:02 +0300

    trash

--- 3/task.rb
+++ 3/task.rb
@@ -1,27 +1,53 @@
 class Station
+  attr_reader :trains
+
   def initialize(name)
-      @name = name
-      @trains = []
+    @name = name
+    @trains = []
+    @gruz_quantity = 0
+    @pass_quantuty = 0
   end
 
-  def train_arrive
-    
+  def train_arrive(train)
+    trains.push(train)
+  end
+
+  def train_departure
+    trains.delete(trains.first)
   end
 end
 
 class Route
+  attr_reader :stops
+
+  def initialize(start_station, final_station)
+    @start_station = start_station
+    @final_station = final_station
+    @stops = [@start_station, @final_station]
+  end
+
+  def add_stops(station)
+    stops.delete(stops.last)
+    stops.push(station)
+    stops.push(@final_station)
+  end 
+
+  def delete_stops
+    stops.delete(stops.last)
+    stops.delete(stops.last)
+    stops.push(@final_station)
+  end 
 end
 
 class Train
+  attr_reader :speed, :quantity , :route_index, :route
+
   def initialize(number, type, quantity)
     @number = number
     @type = type
     @quantity = quantity
     @speed = 0
-  end
-
-  def current_speed
-    @speed
+    @station
   end
 
   def set_speed
@@ -32,22 +58,45 @@ class Train
     @speed = 0
   end
 
-  def quantity
-    @quantity
-  end 
-
   def quantity_attach
     if @speed == 0
       @quantity += 1
-      else @quantity = quantity
+    else 
+      @quantity
     end
   end
 
   def quantity_detach
-    if @quantity > 0 and @speed == 0
+    if @quantity >= 0 and @speed == 0
       @quantity -= 1
-      else @quantity = quantity
+    else 
+      @quantity
     end
   end
+
+  def set_route(route)
+    @route = route
+    @route_index = 0
+  end
+
+  def current_station
+    route.stops[route_index]
+  end
+
+  def move_forward
+    @route_index += 1
+  end
+
+  def move_backward
+     @route_index -= 1
+   end
+
+   def previos
+    route.stops[route_index - 1]
+   end
+
+     def next
+    route.stops[route_index + 1]
+   end
 end
 
